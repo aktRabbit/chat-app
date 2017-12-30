853a3163968f1b92d0b40a84ccdf6b84906906f8
aktRabbit <ankit6480@gmail.com>
2017-12-30 22:31:54 +0530

    modification to newMessage

--- server/server.js
+++ server/server.js
@@ -32,8 +32,8 @@ io.on('connection',(socket)=>
     users.addUser(socket.id, params.name, params.room);
 
     io.to(params.room).emit('updateUserList', users.getUserList(params.room));
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
-    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
+    socket.emit('newMessage', generateMessage(`${params.name}`, 'Welcome to the chat app'));
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage(`${params.name}`, `${params.name} has joined.`));
     callback();
   });
 
